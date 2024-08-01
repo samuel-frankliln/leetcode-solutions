d3c64379409e5e8daebe639f472f207ae07c7dcd
samuel-frankliln <44821150+samuel-frankliln@users.noreply.github.com>
2024-08-01 14:56:31 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for detail in details:
+            age = int(detail[11:13])
+            if age >= 60:
+                count += 1
+        return count
